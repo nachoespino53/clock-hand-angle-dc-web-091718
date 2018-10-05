491eb4e4abdef7cafa04d5d641a77243c99b74a5
nachoespino53 <nachoespino53@gmail.com>
2018-10-05 09:27:38 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,12 @@
 def clock_angle(time)
-    # code your solution here
+    array = time.split(':')
+    array[0] = array[0].to_f
+    array[1] = array[1].to_f
+    small_hand_angle = ((array[0] * 30) + (array[1] / 2))
+    large_hand_angle = (array[1] * 6)
+    if array[0] > 6 && array[1] < 30
+        (small_hand_angle - large_hand_angle-360).abs
+    else
+        (small_hand_angle - large_hand_angle).abs
+    end
 end
